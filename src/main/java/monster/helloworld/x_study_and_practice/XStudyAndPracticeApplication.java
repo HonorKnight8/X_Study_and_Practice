4f1607ae976acb867ceeee2bb42d27c676c8df7e
HonorKnight8 <Honor_8Virtues@outlook.com>
2021-09-20 22:08:46 +0800

    20210920_22:08

--- src/main/java/monster/helloworld/x_study_and_practice/XStudyAndPracticeApplication.java
+++ src/main/java/monster/helloworld/x_study_and_practice/XStudyAndPracticeApplication.java
@@ -2,11 +2,20 @@ package monster.helloworld.x_study_and_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 //@EnableWebMvc
-public class XStudyAndPracticeApplication {
+public class XStudyAndPracticeApplication{
+//public class XStudyAndPracticeApplication extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(XStudyAndPracticeApplication.class);
+//    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(XStudyAndPracticeApplication.class, args);
--- src/main/java/monster/helloworld/x_study_and_practice/domain_survey/Constant.java
+++ src/main/java/monster/helloworld/x_study_and_practice/domain_survey/Constant.java
@@ -2,10 +2,10 @@ package monster.helloworld.x_study_and_practice.domain_survey;
 
 public class Constant {
     // 本地测试环境标记(win + IDEA)
-    public static final Boolean IS_LOCAL_TEST_ENV = true;
-//    public static final Boolean IS_LOCAL_TEST_ENV = false;
+//    public static final Boolean IS_LOCAL_TEST_ENV = true;
+    public static final Boolean IS_LOCAL_TEST_ENV = false;
 
-    public static final String DATA_FILE_PARENT = IS_LOCAL_TEST_ENV ? "D:\\test\\domain_survey" : "/home/tomcat/domain_survey";
+    public static final String DATA_FILE_PARENT = IS_LOCAL_TEST_ENV ? "D:\\test\\domain_survey" : "/templates/domain_survey";
     public static final String DATA_FILE_NAME = "dataFile.json";
 
     // http://www.securityspace.com/s_survey/data/202108/domain.html
